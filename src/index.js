b38ae7d99eb19ae4f4a155b20122892e236e3bba
Wzemczak <Wzemczak@gmail.com>
2023-09-09 23:21:07 +0200

    ccs almost done

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './css/main.css';
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open='1']"),
-    closeModalBtn: document.querySelector("[data-modal-close='1']"),
-    modal: document.querySelector("[data-modal='1']"),
+    openModalBtn: document.querySelector("[data-modal-open='buy-now']"),
+    closeModalBtn: document.querySelector("[data-modal-close='buy-now']"),
+    modal: document.querySelector("[data-modal='buy-now']"),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -15,9 +15,9 @@ import './css/main.css';
 })();
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open='2']"),
-    closeModalBtn: document.querySelector("[data-modal-close='2']"),
-    modal: document.querySelector("[data-modal='2']"),
+    openModalBtn: document.querySelector("[data-modal-open='icecream']"),
+    closeModalBtn: document.querySelector("[data-modal-close='icecream']"),
+    modal: document.querySelector("[data-modal='icecream']"),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -29,9 +29,9 @@ import './css/main.css';
 })();
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open='3']"),
-    closeModalBtn: document.querySelector("[data-modal-close='3']"),
-    modal: document.querySelector("[data-modal='3']"),
+    openModalBtn: document.querySelector("[data-modal-open='milkshakes']"),
+    closeModalBtn: document.querySelector("[data-modal-close='milkshakes']"),
+    modal: document.querySelector("[data-modal='milkshakes']"),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -43,9 +43,9 @@ import './css/main.css';
 })();
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open='4']"),
-    closeModalBtn: document.querySelector("[data-modal-close='4']"),
-    modal: document.querySelector("[data-modal='4']"),
+    openModalBtn: document.querySelector("[data-modal-open='ice-coffe']"),
+    closeModalBtn: document.querySelector("[data-modal-close='ice-coffe']"),
+    modal: document.querySelector("[data-modal='ice-coffe']"),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
@@ -57,9 +57,37 @@ import './css/main.css';
 })();
 (() => {
   const refs = {
-    openModalBtn: document.querySelector("[data-modal-open='5']"),
-    closeModalBtn: document.querySelector("[data-modal-close='5']"),
-    modal: document.querySelector("[data-modal='5']"),
+    openModalBtn: document.querySelector("[data-modal-open='read-more']"),
+    closeModalBtn: document.querySelector("[data-modal-close='read-more']"),
+    modal: document.querySelector("[data-modal='read-more']"),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('visually-hidden');
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open='our-locations']"),
+    closeModalBtn: document.querySelector("[data-modal-close='our-locations']"),
+    modal: document.querySelector("[data-modal='our-locations']"),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('visually-hidden');
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open='francise']"),
+    closeModalBtn: document.querySelector("[data-modal-close='francise']"),
+    modal: document.querySelector("[data-modal='francise']"),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
