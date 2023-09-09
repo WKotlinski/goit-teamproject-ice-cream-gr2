dbb7217ea1ad8d7a3f7837b28753126402d0fef5
Wzemczak <Wzemczak@gmail.com>
2023-09-09 16:17:12 +0200

    script

--- src/index.js
+++ src/index.js
@@ -1,1 +1,71 @@
 import './css/main.css';
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open='1']"),
+    closeModalBtn: document.querySelector("[data-modal-close='1']"),
+    modal: document.querySelector("[data-modal='1']"),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('visually-hidden');
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open='2']"),
+    closeModalBtn: document.querySelector("[data-modal-close='2']"),
+    modal: document.querySelector("[data-modal='2']"),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('visually-hidden');
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open='3']"),
+    closeModalBtn: document.querySelector("[data-modal-close='3']"),
+    modal: document.querySelector("[data-modal='3']"),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('visually-hidden');
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open='4']"),
+    closeModalBtn: document.querySelector("[data-modal-close='4']"),
+    modal: document.querySelector("[data-modal='4']"),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('visually-hidden');
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open='5']"),
+    closeModalBtn: document.querySelector("[data-modal-close='5']"),
+    modal: document.querySelector("[data-modal='5']"),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('visually-hidden');
+  }
+})();
