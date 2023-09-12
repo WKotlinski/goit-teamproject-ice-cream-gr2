c05b0742e27543229a97a3af892bffdd4b7d8990
WKotlinski <132941585+WKotlinski@users.noreply.github.com>
2023-09-12 17:10:54 +0000

    Deploying to gh-pages from @ WKotlinski/goit-teamproject-ice-cream-gr2@5564709a3738899bc1b236cb8e8ff22a13c3f2b9 🚀

---
+++ index.e45383ad.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open='8']"),closeModalBtn:document.querySelector("[data-modal-close='8']"),modal:document.querySelector("[data-modal='8']")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.e45383ad.js.map
